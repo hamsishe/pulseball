3b2a87af6c2470dc954532e1e1ba3295f3cd468c
Hassanmir92 <hassanmir92@gmail.com>
2016-01-06 14:45:42 +0000

    creating new form for adding a new match

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
-var PULSEBALL = new PulseBall();
-var initialRankingTable = [{ "team": { "name": "Australia", "id": 32 }, "pos": 1, "pts": 54.23 },{ "team": { "name": "New Zealand", "id": 62 }, "pos": 2, "pts": 54.00 },{ "team": { "name": "England", "id": 1 }, "pos": 4, "pts": 53.68 }, { "team": { "name": "France", "id": 2 }, "pos": 3, "pts": 51.59 }, { "team": { "name": "Romania", "id": 24 }, "pos": 5, "pts": 43.50 }];
-
 $(function(){
+  var PULSEBALL = new PulseBall();
+  var initialRankingTable = [{ "team": { "name": "Australia", "id": 32 }, "pos": 1, "pts": 54.23 },{ "team": { "name": "New Zealand", "id": 62 }, "pos": 2, "pts": 54.00 },{ "team": { "name": "England", "id": 1 }, "pos": 4, "pts": 53.68 }, { "team": { "name": "France", "id": 2 }, "pos": 3, "pts": 51.59 }, { "team": { "name": "Romania", "id": 24 }, "pos": 5, "pts": 43.50 }];
+
   PULSEBALL.init(initialRankingTable);
   PULSEBALL.rankingsTable.forEach(function(object){
-    $('#ranking-table').append('<tr> <td>' + object.team.name + '</td>' + '<td>' + object.pts.toFixed(2) + '</td></tr>')
-  })
+    $('#ranking-table').append('<tr> <td>' + object.team.name + '</td>' + '<td>' + object.pts.toFixed(2) + '</td></tr>');
+  });
 
+  $('#new-match').on('submit', function(){
+    event.preventDefault();
+    console.log("Submitted");
+  });
 });
