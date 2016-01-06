e323658748e24b1799efc94a663b442257d849e4
Hassanmir92 <hassanmir92@gmail.com>
2016-01-06 15:39:05 +0000

    getting ranking table position for adding the new match

--- js/pulseball.js
+++ js/pulseball.js
@@ -5,3 +5,21 @@ var PulseBall = function(){
 PulseBall.prototype.init = function(rankingJson){
   this.rankingsTable = rankingJson;
 };
+
+PulseBall.prototype.addMatch = function(match){
+  var firstTeam  = match.teams[0].name;
+  var secondTeam = match.teams[1].name;
+  var outcome    = match.outcome;
+  // var ratingDifference = 
+  console.log(this.rankingTablePosition(firstTeam));
+  console.log(this.rankingTablePosition(secondTeam));
+
+};
+
+PulseBall.prototype.rankingTablePosition = function(team){
+  for (var i = 0; i < this.rankingsTable.length; i++) {
+    if(this.rankingsTable[i].team.name === team){
+      return i;
+    };
+  };
+};
