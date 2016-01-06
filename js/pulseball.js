11ee403cc565675ef9971417d3a1157e72b11131
Hassanmir92 <hassanmir92@gmail.com>
2016-01-06 16:06:27 +0000

    able to get ratingDifference based on host

--- js/pulseball.js
+++ js/pulseball.js
@@ -7,12 +7,15 @@ PulseBall.prototype.init = function(rankingJson){
 };
 
 PulseBall.prototype.addMatch = function(match){
-  var firstTeam  = match.teams[0].name;
-  var secondTeam = match.teams[1].name;
-  var outcome    = match.outcome;
-  // var ratingDifference = 
-  console.log(this.rankingTablePosition(firstTeam));
-  console.log(this.rankingTablePosition(secondTeam));
+  var firstTeam          = match.teams[0].name;
+  var secondTeam         = match.teams[1].name;
+  var outcome            = match.outcome;
+  var host               = match.venue.country
+  var firstTeamPosition  = this.rankingTablePosition(firstTeam)
+  var secondTeamPosition = this.rankingTablePosition(secondTeam)
+  var ratingDifference   = this.ratingDifference(firstTeamPosition, secondTeamPosition, host)
+
+  console.log(ratingDifference);
 
 };
 
@@ -22,4 +25,14 @@ PulseBall.prototype.rankingTablePosition = function(team){
       return i;
     };
   };
+};
+
+PulseBall.prototype.ratingDifference = function(firstTeam, secondTeam, host){
+  if(this.rankingsTable[firstTeam].team.name === host){
+    return parseFloat(((this.rankingsTable[firstTeam].pts+3) - this.rankingsTable[secondTeam].pts).toFixed(2))
+  }else if(this.rankingsTable[firstTeam].team.name === host){
+    return parseFloat(((this.rankingsTable[firstTeam].pts) - this.rankingsTable[secondTeam].pts+3).toFixed(2))
+  }else{
+    return parseFloat(((this.rankingsTable[firstTeam].pts) - this.rankingsTable[secondTeam].pts).toFixed(2))
+  }
 };
