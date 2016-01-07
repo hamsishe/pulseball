ad50932dd13da8d584e3953025bb5a138629b8ed
Hassanmir92 <hassanmir92@gmail.com>
2016-01-07 01:01:29 +0000

    created new function to capitalize users inputs for country names

--- js/app.js
+++ js/app.js
@@ -24,6 +24,10 @@ var findCity = function(country){
   };
 };
 
+var capitalize = function(name){
+  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+};
+
 $(function(){
   PULSEBALL.init(initialRankingTable);
   appendNewRankingTable();
@@ -31,9 +35,9 @@ $(function(){
   $('#new-match').on('submit', function(){
     event.preventDefault();
 
-    var firstTeam   = $('#first-team').val();
-    var secondTeam  = $('#second-team').val();
-    var hostCountry = $('#host-country').val();
+    var firstTeam   = capitalize($('#first-team').val());
+    var secondTeam  = capitalize($('#second-team').val());
+    var hostCountry = capitalize($('#host-country').val());
     var outcome     = $('#outcome').val();
 
     var newMatch = {"matchId": 2524, "description": "Match 2", "venue": { "id": 900, "name": "Stadium", "city": findCity(hostCountry), "country": hostCountry}, "teams": [{"id": 2, "name": firstTeam, "abbreviation": firstTeam.substring(0,3).toUpperCase() }, { "id": 1, "name": secondTeam, "abbreviation": secondTeam.substring(0,3).toUpperCase() } ], "scores": [ 19, 23 ], "status": "C", "outcome": outcome };
